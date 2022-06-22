0327fe2707485840f8ea439a703e58f83a57c0c7
Ansh Shah <85743845+Anshshah-1609@users.noreply.github.com>
2022-06-22 11:28:55 +0530

    Add files via upload

---
+++ routes/show.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const File = require('../models/file')
+
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const file = await File.findById(id)
+        if(!file) {
+            return res.status(404).render('download', { error: "File not found!"})
+        }
+        // console.log(file)
+        return res.render('download', {
+            id: file._id,
+            fileName: file.filename,
+            fileSize: (file.size/1000000).toFixed(2),
+            download: `${process.env.APP_BASE_URL}/files/download/${file._id}`
+        })
+    } catch (error) {
+        return res.render('download', { error: "Something went wrong!"})
+    }
+})
+
+module.exports = router
